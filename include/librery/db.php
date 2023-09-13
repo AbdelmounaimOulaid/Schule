7c908ea086bf83b620e0de70ebba3f2ef26541d0
Abdelmounaim Oulaid <97753179+AbdelmounaimOulaid@users.noreply.github.com>
2023-09-13 15:37:49 +0100

    Update db.php

--- include/librery/db.php
+++ include/librery/db.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'schuflqd_schuflqd');//hoch1987_hoch
-define('DB_PASSWORD', 'y46rX452o%f67_MW');//a9]lO^XTQ}P0
-define('DB_NAME', 'schuflqd_schuflqd');//klassen1987_institute
+define('DB_USERNAME', 'schubxdf_schule');//hoch1987_hoch
+define('DB_PASSWORD', 'schule@1987@');//a9]lO^XTQ}P0
+define('DB_NAME', 'schubxdf_schule');//klassen1987_institute
  
 // Connexion à la base de données MySQL 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
